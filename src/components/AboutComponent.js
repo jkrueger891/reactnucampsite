0394cf7447995523d9cbed754443bfba3df33583
jkrueger891 <krueger891@gmail.com>
2021-05-01 12:21:12 -0700

    adding in Partners (check in plural and singular areas)

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { baseUrl } from '../shared/baseUrl';
+// import { PARTNERS } from '../shared/partners';
+import {Loading} from './LoadingComponent';
 
 function About(props) {
 
-    const partners = props.partners.map(partner => {
-        return (
-            <Media tag="li" key={partner.id}>
-                <RenderPartner partner={partner} />
-            </Media>
-        );
-    });
+
 
     return (
         <div className="container">
@@ -65,11 +61,11 @@ function About(props) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
+                {/* <div className="col mt-4">
+                    <Media list> */}
+                        <PartnerList partners={props.partners} />
+                    {/* </Media>
+                </div> */}
             </div>
         </div>
     );
@@ -80,7 +76,7 @@ function RenderPartner({ partner }) {
     if (partner) {
         return (
             <React.Fragment>
-                <Media object src={partner.image} alt={partner.name} width="150" />
+                <Media object src={baseUrl + partner.image} alt={partner.name} width="150" />
                 <Media body className="ml-5 mb-4">
                     <Media heading>{partner.name}</Media>
                     {partner.description}
@@ -91,6 +87,39 @@ function RenderPartner({ partner }) {
     return (
         <div></div>
     );
+}
+
+function PartnerList(props) {
+
+    const partners = props.partners.map(partner => {
+
+        return (
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
+        );
+    });
+
+    if (props.isLoading) {
+        return (
+            <Loading />
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="col">
+                <h4>{props.errMess}</h4>
+            </div>
+
+        );
+    }
+    return (
+        <div className="col mt-4">
+            <Media list>
+                {partners}
+            </Media>
+        </div>
+    )
 }
 
 export default About;
